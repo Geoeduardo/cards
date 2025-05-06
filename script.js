e527de4c91bb29beee16289710b0592f9c7944c8
Geoeduardo <jedu99@outlook.com>
2025-05-05 20:35:47 -0500

    Create the generateCards() function responsible for dynamically creating the card elements

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ const startbtn = document.getElementById('startbtn');
 const gameContainer = document.getElementById('game-container');
 const scoreElement = document.getElementById('score');
 const timerElement = document.getElementById('timer');
+
+function generateCards() {
+    for (const color of cards) {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.dataset.color = color;
+        card.textContent = '?';
+        gameContainer.appendChild(card);
+    }
+}
