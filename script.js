3542e9ebefe74db545eb49fea3ebef3aae7ea48f
Geoeduardo <jedu99@outlook.com>
2025-05-06 17:56:58 -0500

    The shuffle() Function added

--- script.js
+++ script.js
@@ -20,12 +20,10 @@ function generateCards() {
     }
 }
 
-function generateCards() {
-    for (const color of cards) {
-        const card = document.createElement('div');
-        card.classList.add('card');
-        card.dataset.color = color;
-        card.textContent = '?';
-        gameContainer.appendChild(card);
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
+    return array;
 }
